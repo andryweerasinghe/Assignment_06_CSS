b91e006d11181b7cac52d994182a28298bb463bb
andryweerasinghe <andryweerasinghe@gmail.com>
2024-06-05 00:13:45 +0530

    Adding homepageController.js

---
+++ controller/homepageController.js
@@ -0,0 +1,18 @@
+import {orders,items,customers} from "../db/db.js";
+
+$('#orders-section,#customers-section,#items-section').hidden();
+
+$(document).ready(function(){
+   $('.current-page-button').css({
+       background: '#B05200',
+       color: '#FFEEE2',
+       padding: '18px 28px',
+       border: '30px',
+       text: 'none',
+       font: '1000',
+       cursor:'pointer'
+   });
+   $('.Orders,.Customers,.Items').css({
+      cursor:'pointer',
+   });
+});
