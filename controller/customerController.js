661ab1543a803d074ad62542589c7633a15e1788
andryweerasinghe <andryweerasinghe@gmail.com>
2024-06-06 22:33:41 +0530

    clearAll function

--- controller/customerController.js
+++ controller/customerController.js
@@ -61,4 +61,38 @@ var validCustomerId = $('#customers-content-card-left>#txtCustomerID');
 var validCustomerName = $('#customers-content-card-left>#txtName');
 var validCustomerAddress = $('#customers-content-card-left>#txtAddress');
 var validCustomerNo = $('#customers-content-card-left>#txtPhoneNumber');
-var isValidPhoneNumber =
+var isValidPhoneNumber = new RegExp("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$");
+
+$(validCustomerId).on("input", function (event) {
+    $(validCustomerId).css({
+        border: "2px solid #B05200"
+    });
+});
+
+$(validCustomerName).on("input", function (event) {
+    $(validCustomerName).css({
+        border: "2px solid #B05200"
+    });
+});
+
+$(validCustomerAddress).on("input", function (event) {
+    $(validCustomerAddress).css({
+        border: "2px solid #B05200"
+    });
+});
+
+$(validCustomerNo).on("input", function (event) {
+    $(validCustomerNo).css({
+        border: "2px solid #B05200"
+    });
+});
+
+function clearAll(){
+    var customerId = $('#txtCustomerID').val("");
+    var customerName = $('#txtName').val("");
+    var customerAddress = $('#txtAddress').val("");
+    var customerNo = $('#txtPhoneNumber').val("");
+
+    var isValidCustomerName = new RegExp("\\b[A-Z][a-z]*( [A-Z][a-z]*)*\\b");
+    var isValidCustomerAddress = new RegExp("^[A-Za-z0-9'\\/\\.,\\s]{5,}$");
+}
